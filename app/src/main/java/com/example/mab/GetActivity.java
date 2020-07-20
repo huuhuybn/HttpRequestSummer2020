48dc3ceec53983f405a233a79f56c70f08c40f52
HuyNguyen <huuhuybn@gmail.com>
2020-07-20 13:41:28 +0700

    Full code

--- app/src/main/java/com/example/mab/GetActivity.java
+++ app/src/main/java/com/example/mab/GetActivity.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
@@ -15,9 +22,11 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class GetActivity extends AppCompatActivity {
@@ -57,22 +66,44 @@ public class GetActivity extends AppCompatActivity {
 //                    }
 //                });
 
-                AndroidNetworking.get("http://dotplays.com/wp-json/")
-                        .build().getAsJSONObject(new JSONObjectRequestListener() {
+//                AndroidNetworking.get("http://dotplays.com/wp-json/")
+//                        .build().getAsJSONObject(new JSONObjectRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        MyModel myModel = new Gson().fromJson(response.toString(),MyModel.class);
+//
+//                        tvText.setText(myModel.description);
+//                    }
+//
+//                    @Override
+//                    public void onError(ANError anError) {
+//
+//                    }
+//                });
+
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(GetActivity.this);
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, link,
+                        new com.android.volley.Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                MyModel myModel = new Gson().fromJson(response, MyModel.class);
+                                tvText.setText(myModel.description);
+                            }
+                        }
+                        , new Response.ErrorListener() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        MyModel myModel = new Gson().fromJson(response.toString(),MyModel.class);
-
-                        tvText.setText(myModel.description);
-                    }
-
-                    @Override
-                    public void onError(ANError anError) {
+                    public void onErrorResponse(VolleyError error) {
 
                     }
-                });
+                }
+                );
 
-
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
 
--- app/src/main/java/com/example/mab/PostActivity.java
+++ app/src/main/java/com/example/mab/PostActivity.java
@@ -7,9 +7,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class PostActivity extends AppCompatActivity {
@@ -36,24 +52,72 @@ public class PostActivity extends AppCompatActivity {
                 String body = edtBody.getText().toString().trim();
                 String title = edtTitle.getText().toString().trim();
 
-                Retrofit retrofit =
-                        RestRetrofit.getInstance("https://jsonplaceholder.typicode.com");
+//                Retrofit retrofit =
+//                        RestRetrofit.getInstance("https://jsonplaceholder.typicode.com");
+//
+//                RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+//
+//                retrofitService.getPostData(title, id, body).enqueue(new Callback<PostModel>() {
+//                    @Override
+//                    public void onResponse(Call<PostModel> call, Response<PostModel> response) {
+//                        PostModel postModel = response.body();
+//                        textView.setText(postModel.userId + ":" + postModel.title + ":"
+//                        + postModel.body);
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<PostModel> call, Throwable t) {
+//
+//                    }
+//                });
 
-                RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+//                AndroidNetworking.post("https://jsonplaceholder.typicode.com/posts")
+//                        .addBodyParameter("title",title).addBodyParameter("body",body)
+//                        .addBodyParameter("userId",id).build()
+//                        .getAsJSONObject(new JSONObjectRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        PostModel postModel =
+//                                new Gson().fromJson(response.toString(),PostModel.class);
+//                        textView.setText(postModel.body);
+//                    }
+//
+//                    @Override
+//                    public void onError(ANError anError) {
+//
+//                    }
+//                });
 
-                retrofitService.getPostData(title, id, body).enqueue(new Callback<PostModel>() {
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(PostActivity.this);
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, link,
+                        new com.android.volley.Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                PostModel myModel = new Gson().fromJson(response, PostModel.class);
+                                textView.setText(myModel.body);
+
+                            }
+                        }
+                        , new Response.ErrorListener() {
                     @Override
-                    public void onResponse(Call<PostModel> call, Response<PostModel> response) {
-                        PostModel postModel = response.body();
-                        textView.setText(postModel.userId + ":" + postModel.title + ":"
-                        + postModel.body);
-                    }
+                    public void onErrorResponse(VolleyError error) {
 
+                    }
+                }
+                ){
                     @Override
-                    public void onFailure(Call<PostModel> call, Throwable t) {
-
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("title", title);
+                        params.put("body", body);
+                        params.put("userId", id);
+                        return params;
                     }
-                });
+                };
+                queue.add(stringRequest);
 
 
             }
