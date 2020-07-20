30c67593dc93557d3ca30fc606f18bbcd9db38e6
HuyNguyen <huuhuybn@gmail.com>
2020-07-20 13:15:15 +0700

    bbb

--- app/src/main/java/com/example/mab/GetActivity.java
+++ app/src/main/java/com/example/mab/GetActivity.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,25 +34,40 @@ public class GetActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Retrofit restRetrofit = RestRetrofit.getInstance("http://dotplays.com");
-                RetrofitService retrofitService = restRetrofit.create(RetrofitService.class);
+//                Retrofit restRetrofit = RestRetrofit.getInstance("http://dotplays.com");
+//                RetrofitService retrofitService = restRetrofit.create(RetrofitService.class);
+//
+//                retrofitService.getData().
+//                        enqueue(new Callback<MyModel>() {
+//                    @Override
+//                    public void onResponse
+//                            (Call<MyModel> call,
+//                             Response<MyModel> response) {
+//                        MyModel myModel = response.body();
+//                        Toast.makeText(GetActivity.this,
+//                                myModel.home,
+//                                Toast.LENGTH_SHORT).show();
+//
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<MyModel> call, Throwable t) {
+//
+//                    }
+//                });
 
-                retrofitService.getData().
-                        enqueue(new Callback<MyModel>() {
+                AndroidNetworking.get("http://dotplays.com/wp-json/")
+                        .build().getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
-                    public void onResponse
-                            (Call<MyModel> call,
-                             Response<MyModel> response) {
-                        MyModel myModel = response.body();
-                        Toast.makeText(GetActivity.this,
-                                myModel.home,
-                                Toast.LENGTH_SHORT).show();
+                    public void onResponse(JSONObject response) {
+                        MyModel myModel = new Gson().fromJson(response.toString(),MyModel.class);
 
-
+                        tvText.setText(myModel.description);
                     }
 
                     @Override
-                    public void onFailure(Call<MyModel> call, Throwable t) {
+                    public void onError(ANError anError) {
 
                     }
                 });
